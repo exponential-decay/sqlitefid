969c4fb06e6a05ef79c9abb3537cbf0b993cc608
ross-spencer <all.along.the.watchtower2001@gmail.com>
2017-05-26 22:09:39 +1200

    [ignore] name.

--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 class SqliteFIDVersion:
 
-   version = 'sqlitfid-1.0.0' #need something reasonable here...
+   version = 'sqlitefid-1.0.0' #need something reasonable here...
    
    def getVersion(self):
       return self.version
