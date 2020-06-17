ee356832c5881cd33cba243a022deb2bd9dd8b9d
Ross Spencer <all.along.the.watchtower2001@gmail.com>
2020-06-16 22:47:01 -0400

    Apply linting changes to codebase
    In preparation for migrating to Python 3 we run Black, isort, Flake8,
    and Pylint to find the initial changes that should be made to the
    code to improve formatting and layout.

--- libs/Version.py
+++ libs/Version.py
@@ -1,7 +1,9 @@
+# -*- coding: utf-8 -*-
+
+
 class SqliteFIDVersion:
 
-   version = 'sqlitefid-1.0.0' #need something reasonable here...
-   
-   def getVersion(self):
-      return self.version
-   
+    version = "sqlitefid-1.0.0"  # need something reasonable here...
+
+    def getVersion(self):
+        return self.version
