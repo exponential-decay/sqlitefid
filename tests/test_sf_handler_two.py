e5c9793ae19a288626711fa3a585b90b5f1d6d10
Ross Spencer <all.along.the.watchtower2001@gmail.com>
2021-07-18 08:43:15 +0200

    Fixup SF Handler Tests

--- tests/test_sf_handler_two.py
+++ tests/test_sf_handler_two.py
@@ -4,8 +4,6 @@
 
 import sys
 
-import pytest
-
 from sqlitefid.libs.SFHandlerClass import SFYAMLHandler
 
 SIEGFRIED_YAML = u"""---
@@ -128,7 +126,7 @@ matches  :
     basis   : 'extension match pdf; byte match at 0, 4'
     warning :
 ---
-filename : 'Q🖤42591XX.mp3'
+filename : 'Q42591🖤.mp3'
 filesize : 3
 modified : 2021-07-08T23:21:40+02:00
 errors   :
@@ -169,28 +167,148 @@ else:
 
 
 def test_read_sf_yaml(tmp_path):
+    """Ensure that the SF data above can be properly parsed and outputs
+    sensible data.
+    """
 
     dir_ = tmp_path
     sf_yaml = dir_ / "sf_test.yaml"
     sf_yaml.write_text(SIEGFRIED_YAML.strip())
 
     sf = SFYAMLHandler()
-    a = sf.readSFYAML(str(sf_yaml))
+    sf.readSFYAML(str(sf_yaml))
 
-    # print(a)
-    # print(sf.sfdata)
-    # print(sf.PROCESSING_ERROR)
-    # assert sf.sectioncount == 5
+    assert sf.sectioncount == 5
     paths = [u"{}".format(f["filename"]) for f in sf.files]
 
-    print(paths)
+    assert (
+        list(set(paths)).sort()
+        == ["Q10287816.gz", "Q42591🖤.mp3", "Q42332.pdf", "Q28205479.info"].sort()
+    )
 
-    # print(len(paths))
-    # print(set(paths))
+    files = sf.sfdata["files"]
 
-    # if PY3:
-    # assert set(paths) == {'Q10287816.gz', 'Q42591🖤.mp3', 'Q42332.pdf', 'Q28205479.info'}
-    # else:
-    # 	assert set(paths) == {u'Q42591\U0001f5a4.mp3', u'Q42332.pdf', u'Q28205479.info', u'Q10287816.gz', u'Q42591.mp3'}
+    assert len(sf.files) == 4
+    assert len(files) == 4
 
-    assert False
+    assert files[0] == {
+        "filename": "Q10287816.gz",
+        "filesize": "3",
+        "modified": "2021-05-24T19:26:56+02:00",
+        "errors": "",
+        "md5": "613ffd2ae0a8828aa573ce62bf2e30c3",
+        "type": "Container",
+        "containertype": "gz",
+        "uri": "gz:file:///Q10287816.gz!",
+        "uri scheme": "gz",
+        "identification": {
+            "pronom": {
+                "id": "x-fmt/266",
+                "format": "GZIP Format",
+                "version": "",
+                "mime": "application/gzip",
+                "method": "Signature",
+                "basis": "extension match gz; byte match at 0, 3",
+                "warning": None,
+            },
+            "tika": {
+                "id": "application/gzip",
+                "format": "Gzip Compressed Archive",
+                "mime": "application/gzip",
+                "method": "Signature",
+                "basis": "extension match gz; byte match at 0, 2 (signature 1/2); byte match at 0, 2 (signature 2/2)",
+                "warning": None,
+                "version": "",
+            },
+            "freedesktop.org": {
+                "id": "application/gzip",
+                "format": "Gzip archive",
+                "mime": "application/gzip",
+                "method": "Signature",
+                "basis": "extension match gz; byte match at 0, 2",
+                "warning": None,
+                "version": "",
+            },
+            "loc": {
+                "id": "UNKNOWN",
+                "format": "",
+                "mime": "none",
+                "basis": None,
+                "method": "None",
+                "extension mismatch": False,
+                "warning": "no match",
+                "version": "",
+            },
+        },
+    }
+
+    assert files[3] == {
+        "filename": u"Q42591🖤.mp3",
+        "filesize": "3",
+        "modified": "2021-07-08T23:21:40+02:00",
+        "errors": "",
+        "md5": "c0f44879dc0d4eae7b3f0b3e801e373c",
+        "type": "File",
+        "uri": "file:///Q42591🖤.mp3",
+        "uri scheme": "file",
+        "identification": {
+            "pronom": {
+                "id": "UNKNOWN",
+                "format": "",
+                "version": "",
+                "mime": "none",
+                "basis": None,
+                "method": "None",
+                "extension mismatch": False,
+                "warning": "no match; possibilities based on extension are fmt/134",
+            },
+            "tika": {
+                "id": "audio/mpeg",
+                "format": "MPEG-1 Audio Layer 3",
+                "mime": "audio/mpeg",
+                "method": "Signature",
+                "basis": "extension match mp3; byte match at 0, 3 (signature 12/12)",
+                "warning": None,
+                "version": "",
+            },
+            "freedesktop.org": {
+                "id": "audio/mpeg",
+                "format": "MP3 audio",
+                "mime": "audio/mpeg",
+                "method": "Signature",
+                "basis": "extension match mp3; byte match at 0, 3 (signature 2/2)",
+                "warning": None,
+                "version": "",
+            },
+            "loc": {
+                "id": "UNKNOWN",
+                "format": "",
+                "mime": "none",
+                "basis": None,
+                "method": "None",
+                "extension mismatch": False,
+                "warning": "no match; possibilities based on extension are fdd000052, fdd000053, fdd000105, fdd000111, fdd000256, fdd000275",
+                "version": "",
+            },
+        },
+    }
+
+    assert sf.sfdata["header"] == {
+        "siegfried": "1.9.1",
+        "scandate": "2021-07-17T22:11:59+02:00",
+        "signature": "default.sig",
+        "created": "2020-10-06T19:15:15+02:00",
+        "id namespace 1": "pronom",
+        "id details 1": "DROID_SignatureFile_V97.xml; container-signature-20201001.xml",
+        "identifier count": 4,
+        "id namespace 2": "tika",
+        "id details 2": "tika-mimetypes.xml (1.24, 2020-04-17)",
+        "id namespace 3": "freedesktop.org",
+        "id details 3": "freedesktop.org.xml (2.0, 2020-06-05)",
+        "id namespace 4": "loc",
+        "id details 4": "fddXML.zip (2020-09-02, DROID_SignatureFile_V97.xml, container-signature-20201001.xml)",
+    }
+
+    assert sf.hashtype == "md5"
+    assert sf.identifiercount == 4
+    assert sf.filecount == 4
